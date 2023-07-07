7865f1a4a19a737cd18c17d67c841eb2b44289e1
Jean Tadebois <jpm.tadebois@gmail.com>
2023-07-07 13:24:23 +1000

    Successfully achieved Theming the app with Jetpack Compose
    Where the `LemonadeLabTheme` Composable is declared in `Theme.kt`, it is important to check that the Theme properly calls the app's colour palette.

--- app/src/main/java/me/tadebois/lemonadelab/ui/theme/Color.kt
+++ app/src/main/java/me/tadebois/lemonadelab/ui/theme/Color.kt
@@ -2,10 +2,12 @@ package me.tadebois.lemonadelab.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val md_theme_light_primaryContainer = Color(0xFFF9E44C)
+val md_theme_light_onPrimaryContainer = Color(0xFF201C00)
+val md_theme_light_tertiaryContainer = Color(0xFFC3ECD2)
+val md_theme_light_background = Color(0xFFFFFBFF)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val md_theme_dark_primaryContainer = Color(0xFF504700)
+val md_theme_dark_onPrimaryContainer = Color(0xFFF9E44C)
+val md_theme_dark_tertiaryContainer = Color(0xFF294E3B)
+val md_theme_dark_background = Color(0xFF1D1C16)
--- app/src/main/java/me/tadebois/lemonadelab/ui/theme/Theme.kt
+++ app/src/main/java/me/tadebois/lemonadelab/ui/theme/Theme.kt
@@ -15,33 +15,25 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+private val LightColorScheme = lightColorScheme(
+    primaryContainer = md_theme_light_primaryContainer,
+    onPrimaryContainer = md_theme_light_onPrimaryContainer,
+    tertiaryContainer = md_theme_light_tertiaryContainer,
+    background = md_theme_light_background
 )
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
 
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+private val DarkColorScheme = darkColorScheme(
+    primaryContainer = md_theme_dark_primaryContainer,
+    onPrimaryContainer = md_theme_dark_onPrimaryContainer,
+    tertiaryContainer = md_theme_dark_tertiaryContainer,
+    background = md_theme_dark_background,
 )
 
 @Composable
 fun LemonadeLabTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
